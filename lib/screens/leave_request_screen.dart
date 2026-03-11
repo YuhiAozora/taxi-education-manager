11a87515a59be9f37a024d456564af611985d763
Yuhi_Aozora <genspark_flutter_dev@genspark.ai>
2026-03-11 05:46:34 +0000

    🔧 foundation.dart のインポートを追加

--- lib/screens/leave_request_screen.dart
+++ lib/screens/leave_request_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
 import '../models/user.dart';
 import '../models/leave_request.dart';
